9f13046d52d78c3dc89482602b97ed2aa0978e03
Guillaume Docquier <guillaumedocquier@hotmail.com>
2021-09-08 19:46:54 -0400

    Added missing assets for current stonemasonry data

--- src/crowcraft/src/data/index.js
+++ src/crowcraft/src/data/index.js
@@ -3,13 +3,18 @@ import { String } from "utils";
 const items = [];
 const itemsContext = require.context("./items", false, /^(?!.*index).*\.js$/);
 for (const itemFileName of itemsContext.keys()) {
-    let itemId = itemFileName
-        .match(/\/([0-9a-zA-Z-]*)\.js$/)[1]
-        .split("-")
-        .map(String.capitalize)
-        .join("");
+    try {
+        let itemId = itemFileName
+            .match(/\/([0-9a-zA-Z-]*)\.js$/)[1]
+            .split("-")
+            .map(String.capitalize)
+            .join("");
 
-    items.push(itemsContext(itemFileName)[itemId]);
+        items.push(itemsContext(itemFileName)[itemId]);
+    }
+    catch(e) {
+        console.error(itemFileName, e);
+    }
 }
 
 export { items };
@@ -17,8 +22,13 @@ export { items };
 const assets = {};
 const assetsContext = require.context("./assets", false, /\.svg$/);
 for (const assetFileName of assetsContext.keys()) {
-    const assetId = assetFileName.match(/\.\/([0-9a-zA-Z]*)\.svg$/)[1];
-    assets[assetId] = assetsContext(assetFileName).default;
+    try {
+        const assetId = assetFileName.match(/\.\/([0-9a-zA-Z]*)\.svg$/)[1];
+        assets[assetId] = assetsContext(assetFileName).default;
+    }
+    catch(e) {
+        console.error(assetFileName, e);
+    }
 }
 
 export const getAsset = assetId => {
