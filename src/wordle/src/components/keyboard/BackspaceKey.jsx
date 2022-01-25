5348e15bd25447a8ef6c6568630315c1a2c74056
Guillaume Docquier <guillaumedocquier@hotmail.com>
2022-01-24 22:35:02 -0500

    Mobile friendlier

--- src/wordle/src/components/keyboard/BackspaceKey.jsx
+++ src/wordle/src/components/keyboard/BackspaceKey.jsx
@@ -1,8 +1,10 @@
+import "./BackspaceKey.css";
+
 import { ReactComponent as Backspace } from './backspace.svg';
 import { Key } from "./Key";
 
 export const BackspaceKey = ({ onClick }) => {
     return (
-        <Key char={<Backspace />} onClick={onClick} />
+        <Key char={<Backspace className="backspace" />} onClick={onClick} />
     );
 }
