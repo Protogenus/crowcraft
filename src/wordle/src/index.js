48f11b9d73fbe905df0164ba2b9c542d2c970ad8
Guillaume Docquier <guillaumedocquier@hotmail.com>
2022-01-24 22:53:52 -0500

    Fix mobile height

--- src/wordle/src/index.js
+++ src/wordle/src/index.js
@@ -4,12 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Fix for mobile height not taking into account the address bar: https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
